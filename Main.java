ca6f4af5bb5ad1ec2e850e0ba8225449c6d619c7
Jeongwon Kim <gardenk11181@snu.ac.kr>
2020-05-27 05:20:30 +0000

    isAnBn added

--- Main.java
+++ Main.java
@@ -2,10 +2,37 @@ class Main {
   public static void main(String[] args) {
     System.out.println(RecursionMethod.isIdentifier("Hh"));
     System.out.println(RecursionMethod.isPalindrome("HhH"));
+    System.out.println(RecursionMethod.isAnBn("HHBB"));
   }
 }
 
 class RecursionMethod {
+  
+  public static boolean isAnBn(String string) {
+    if(string.length()==0) {
+      return true;
+    } else if(string.length()==1) {
+      return false;
+    } else {
+      return isAnBn(string.substring(1,string.length()-1),string.charAt(0), string.charAt(string.length()-1));
+    }
+  }
+  
+  public static boolean isAnBn(String string, char a, char b) {
+    if(string.length()==0) {
+      return true;
+    } else if(string.length()==1) {
+      return false;
+    } else {
+      if(string.charAt(0)==a && string.charAt(string.length()-1)==b) {
+        return isAnBn(string.substring(1,string.length()-1), a,b);
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  
   public static boolean isIdentifier(String string) {
     if(string.length()==1) {
       if(isLetter(string.charAt(0))) return true;
